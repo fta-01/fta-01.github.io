102491acce149278b1296a5793f64efe1dbacf07
fta-01 <405091602@qq.com>
2025-10-02 17:36:40 +0800

    Site updated: YYYY-10-2025年10月2日 17:36:40

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1759397418133";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1759397798709";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
